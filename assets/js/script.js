10b0262a3514f50713e68abe238524e947fb5f93
Liam Ok <liamokane19@gmail.com>
2022-01-13 15:55:23 +1100

    day 2 working. Manual coding. Need to apply for loop. But for the moment repeated coding is working for furture day forecasts

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,20 @@ var windEl = document.querySelector('.wind');
 var humidityEl = document.querySelector('.humidity');
 
 //future container variables
-var futureNameEl = document.querySelector('.futurename');
-var futuredescEl = document.querySelector('.futuredesc');
-var futuretempEl= document.querySelector('.futuretemp');
-var futurewindEl = document.querySelector('.futurewind');
-var futurehumidityEl = document.querySelector('.futurehumidity');
-var futureuvIndex = document.querySelector('.futureuvIndex');
+var day1futureNameEl = document.querySelector('.futurename1');
+var day1futuredescEl = document.querySelector('.futuredesc1');
+var day1futuretempEl= document.querySelector('.futuretemp1');
+var day1futurewindEl = document.querySelector('.futurewind1');
+var day1futurehumidityEl = document.querySelector('.futurehumidity1');
+var day1futureuvIndex = document.querySelector('.futureuvIndex1');
+
+var day2futureNameEl = document.querySelector('.futurename2');
+var day2futuredescEl = document.querySelector('.futuredesc2');
+var day2futuretempEl= document.querySelector('.futuretemp2');
+var day2futurewindEl = document.querySelector('.futurewind2');
+var day2futurehumidityEl = document.querySelector('.futurehumidity2');
+var day2futureuvIndex = document.querySelector('.futureuvIndex2');
+
 
 button.addEventListener('click', function(event){ 
     event.preventDefault()
@@ -44,20 +52,36 @@ button.addEventListener('click', function(event){
         .then(response => response.json())
         // .then(data => console.log (data))
         .then(data => {
-            var futurenameValue = data ['city']['name'];
-            var futuredescValue = data ['list'][0]['weather'][0]['description'];
-            var futuretempValue = data ['list'][0]['main']['temp'];
-            var futurewindValue = data ['list'][0]['wind']['speed'];
-            var futurehumidityValue = data ['list'][0]['main']['humidity'];
-            // var futureuvIndexValue = data ['id'];
+            //data attribute for furture forecast 
+            var day1futurenameValue = data ['city']['name'];
+            var day1futuredescValue = data ['list'][0]['weather'][0]['description'];
+            var day1futuretempValue = data ['list'][0]['main']['temp'];
+            var day1futurewindValue = data ['list'][0]['wind']['speed'];
+            var day1futurehumidityValue = data ['list'][0]['main']['humidity'];
+            var day1futureuvIndexValue = data ['city']['coord']['lat','lon'];
+
+            day1futureNameEl.innerHTML=day1futurenameValue;
+            day1futuredescEl.innerHTML=day1futuredescValue;
+            day1futuretempEl.innerHTML=day1futuretempValue;
+            day1futurewindEl.innerHTML=day1futurewindValue;
+            day1futurehumidityEl.innerHTML=day1futurehumidityValue;
+            day1futureuvIndex.innerHTML=day1futureuvIndexValue;
+
+            var day2futurenameValue = data ['city']['name'];
+            var day2futuredescValue = data ['list'][1]['weather'][0]['description'];
+            var day2futuretempValue = data ['list'][1]['main']['temp'];
+            var day2futurewindValue = data ['list'][1]['wind']['speed'];
+            var day2futurehumidityValue = data ['list'][1]['main']['humidity'];
+            var day2futureuvIndexValue = data ['city']['coord']['lat','lon'];
+
+            day2futureNameEl.innerHTML=day2futurenameValue;
+            day2futuredescEl.innerHTML=day2futuredescValue;
+            day2futuretempEl.innerHTML=day2futuretempValue;
+            day2futurewindEl.innerHTML=day2futurewindValue;
+            day2futurehumidityEl.innerHTML=day2futurehumidityValue;
+            day2futureuvIndex.innerHTML=day2futureuvIndexValue;
 
 
-            futureNameEl.innerHTML=futurenameValue;
-            futuredescEl.innerHTML=futuredescValue;
-            futuretempEl.innerHTML=futuretempValue;
-            futurewindEl.innerHTML=futurewindValue;
-            futurehumidityEl.innerHTML=futurehumidityValue;
-            // futureuvIndex.innerHTML=futureuvIndexValue;
         })
 
     .catch(err => alert("City Name undefined"))
